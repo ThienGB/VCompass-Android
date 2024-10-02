3d25bbd97107835b760880475826a1c0ebbf63df
dunkvippro <118897462+dunkvippro@users.noreply.github.com>
2024-10-03 00:08:36 +0700

    create profile page

--- GoTravel/app/build.gradle.kts
+++ GoTravel/app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.foundation.layout.android)
     implementation(libs.firebase.firestore)
+    implementation(libs.androidx.ui.test.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
